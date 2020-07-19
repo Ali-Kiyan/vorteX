a0bb8a2c6c29c7ef1cdcbd7f40da053d2f69b262
Ali <alikiyand@gmail.com>
2020-07-19 17:16:50 +0100

    added compnents and contaiers template for ramp material and ramp size

---
+++ client/vort-x/src/components/BoatRampsPerMaterial.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { BoatRampPerMaterialContainer } from '../containers/boat_ramps_per_material_container';
+
+export const BoatRampPerMaterial = (props) => {
+    return <BoatRampPerMaterialContainer />
+}
---
+++ client/vort-x/src/components/BoatRampsPerSize.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { BoatRampPerSizeContainer } from '../containers/boat_ramps_per_size_container';
+
+export const BoatRampPerSize = (props) => {
+    return <BoatRampPerMaterialContainer />
+}
